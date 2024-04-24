55dea047383a6c1c075edf0299b68765aca015fd
Andrey Kerensky <kerensky.git@gmail.com>
2024-04-24 16:47:20 +0400

    fix

--- VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/longpoll/bot/objects/MessageNewObject.kt
+++ VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/longpoll/bot/objects/MessageNewObject.kt
@@ -7,5 +7,5 @@ import ru.krindra.vknorthtypes.types.messages.MessagesMessage
 @Serializable
 data class MessageNewObject(
     @SerialName("message") val message: MessagesMessage,
-    @SerialName("client_info") val clientInfo: ClientInfo,
+    @SerialName("client_info") val clientInfo: ClientInfoObject,
 )
