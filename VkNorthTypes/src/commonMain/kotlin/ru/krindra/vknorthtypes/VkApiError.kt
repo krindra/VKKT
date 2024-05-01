f13de2edb08df48557f58dde84cb5dbd7c1388c0
Andrey Kerensky <kerensky.git@gmail.com>
2024-05-01 12:54:17 +0400

    add fromString function

--- VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/VkApiError.kt
+++ VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/VkApiError.kt
@@ -1,8 +1,16 @@
 package ru.krindra.vknorthtypes
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 class VkApiError(
     val code: Int,
     val description: String
 ): Exception("$description\ncode: $code") {
     operator fun get(code: Int): Boolean = code == this.code
+
+    companion object {
+        fun fromString(string: String): VkApiError =
+            JsonSingleton.json.decodeFromString(string)
+    }
 }
--- VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/longpoll/bot/Event.kt
+++ VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/longpoll/bot/Event.kt
@@ -1,10 +1,16 @@
 package ru.krindra.vknorthtypes.longpoll.bot
 
 import kotlinx.serialization.Serializable
+import ru.krindra.vknorthtypes.JsonSingleton
 import ru.krindra.vknorthtypes.longpoll.bot.events.BaseEvent
 
 @Serializable
 data class Event(
     val ts: String,
     val updates: List<BaseEvent>
-)
+) {
+    companion object {
+        fun fromString(string: String): Event =
+            JsonSingleton.json.decodeFromString(string)
+    }
+}
