3f9f27fa64924744e959d068bc14a9be0e9221d7
Andrey Kerensky <kerensky.git@gmail.com>
2024-06-26 09:45:53 +0400

    add AttachmentType

---
+++ VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/longpoll/user/enums/AttachmentType.kt
@@ -0,0 +1,23 @@
+package ru.krindra.vknorthtypes.longpoll.user.enums
+
+enum class AttachmentType(val value: String) {
+    PHOTO("photo"),
+    STICKER("sticker"),
+    AUDIO("audio"),
+    LINK("link"),
+    VIDEO("video"),
+    DOC("doc"),
+    WALL("wall"),
+    GIFT("gift"),
+    AUDIO_MESSAGE("audio_message"),
+    AUDIO_MSG("audiomsg"),
+    POLL("poll"),
+    GRAFFITI("graffiti"),
+    CALL("call");
+
+    companion object {
+        fun fromString(string: String): AttachmentType {
+            return enumValues<AttachmentType>().first { it.value == string }
+        }
+    }
+}
