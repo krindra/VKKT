aa27ec01ff96a2c53480c87a930ea52b2383fae2
Andrey Kerensky <kerensky.git@gmail.com>
2024-06-26 14:24:17 +0400

    refactor user longpoll

--- VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/longpoll/user/UserLPEvent.kt
+++ VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/longpoll/user/UserLPEvent.kt
@@ -1,8 +1,31 @@
 package ru.krindra.vknorthtypes.longpoll.user
 
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import ru.krindra.vknorthtypes.JsonSingleton
 import ru.krindra.vknorthtypes.longpoll.user.updates.UserLPUpdate
 
 data class UserLPEvent(
     val ts: Int,
     val updates: List<UserLPUpdate>,
-)
+) {
+    fun fromEvent(rawEvent: String): UserLPEvent {
+        val jsonEvent = JsonSingleton.json
+            .parseToJsonElement(rawEvent)
+
+        val ts = jsonEvent
+            .jsonObject["ts"]!!
+            .jsonPrimitive.int
+
+        val rawUpdates = jsonEvent
+            .jsonObject["updates"]!!
+            .jsonArray
+        val updates = mutableListOf<UserLPUpdate>()
+        for (rawUpdate in rawUpdates)
+            updates.add(UserLPUpdate.fromJsonArray(rawUpdate.jsonArray))
+
+        return UserLPEvent(ts, updates)
+    }
+}
