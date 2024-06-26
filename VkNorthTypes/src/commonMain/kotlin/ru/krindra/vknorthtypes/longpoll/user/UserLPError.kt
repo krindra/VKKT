b4a973dd2d80a9df94ba77efab03713fac580b9f
Andrey Kerensky <kerensky.git@gmail.com>
2024-06-26 16:47:18 +0400

    fix

--- VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/longpoll/user/UserLPError.kt
+++ VkNorthTypes/src/commonMain/kotlin/ru/krindra/vknorthtypes/longpoll/user/UserLPError.kt
@@ -6,14 +6,14 @@ import ru.krindra.vknorthtypes.JsonSingleton
 import ru.krindra.vknorthtypes.longpoll.bot.BotLPError
 
 @Serializable
-class UserLPError(
+data class UserLPError(
     val failed: Int,
     val ts: Int? = null,
     @SerialName("min_version") val minVersion: Int? = null,
     @SerialName("max_version") val maxVersion: Int? = null,
 ): Exception("failed code: $failed") {
     companion object {
-        fun fromString(string: String): BotLPError =
+        fun fromString(string: String): UserLPError =
             JsonSingleton.json.decodeFromString(string)
     }
 }
