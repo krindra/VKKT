246d646eab45d720093822251b0f9e0e1c225838
Andrey Kerensky <kerensky.git@gmail.com>
2024-07-15 22:40:58 +0400

    fix decodeResponse

--- methods/src/commonMain/kotlin/ru/krindra/vkkt/methods/decodeResponse.kt
+++ methods/src/commonMain/kotlin/ru/krindra/vkkt/methods/decodeResponse.kt
@@ -1,11 +1,13 @@
 package ru.krindra.vkkt.methods
 
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
+@Serializable
+internal class Response<T>(
+    val response: T
+)
+
 internal inline fun <reified T> decodeResponse(rawResponse: String, json: Json): T {
-    return json.decodeFromString<T>(
-        with(rawResponse) {
-            substring(12..<length - 1)
-        }
-    )
+    return json.decodeFromString<Response<T>>(rawResponse).response
 }
